5f138da8d778fdf7ffd5ee238c8d3fa08eb6f160
Diana-nyamai <dnyamai.dn@gmail.com>
2021-06-02 12:19:31 +0300

    login.js update

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -5,9 +5,9 @@ import bike from '../assets/Images/bike.png'
 const Login = () => {
     return (
         <div>
-           <Grid container>
+           <Grid container style={{height: '100vh'}}>
            <Grid item xs={12} sm={6}>
-        <Img src={bike} alt="left-i" style={{height:'100%', width:'100%', objectFit:'cover'}} />
+        <img src={bike} alt="left-i" style={{height:'100%', width:'100%', objectFit:'cover'}} />
            </Grid>
            <Grid item xs={12} sm={6}></Grid>
            </Grid>
