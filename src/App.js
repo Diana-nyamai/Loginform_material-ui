400a971dce6fe64813a13b44baf33d4a05a9d8f1
Diana-nyamai <dnyamai.dn@gmail.com>
2021-06-02 12:04:38 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Login from './auth/Login';
 
 function App() {
   return (
     <div className="App">
-      
+      <Login/>
     </div>
   );
 }
--- src/auth/Login.js
+++ src/auth/Login.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import {Grid} from '@material-ui/core';
+import bike from '../assets/Images/bike.png'
 
 const Login = () => {
     return (
         <div>
-            
+           <Grid container>
+           <Grid item xs={12} sm={6}>
+        <Img src={bike} alt="left-i" style={{height:'100%', width:'100%', objectFit:'cover'}} />
+           </Grid>
+           <Grid item xs={12} sm={6}></Grid>
+           </Grid>
         </div>
     )
 }
