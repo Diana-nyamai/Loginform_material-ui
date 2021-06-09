3212325f6da7acd00da59e0b7781c865bcc83c97
Diana-nyamai <dnyamai.dn@gmail.com>
2021-06-09 22:46:53 +0300

    homepage update

--- src/pages/Homepage/HomePage.js
+++ src/pages/Homepage/HomePage.js
@@ -1,9 +1,12 @@
+import { Button } from '@material-ui/core'
 import React from 'react'
 
 function HomePage() {
     return (
         <div>
-            <h1>this is the HomePage</h1>
+            <h1>welcome to you page</h1>
+            <Button>Log out</Button>
+
         </div>
     )
 }
