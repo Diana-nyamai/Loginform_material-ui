c1de7e408df252ac5953b10b2808a1f15390bff7
Diana-nyamai <dnyamai.dn@gmail.com>
2021-06-09 23:16:32 +0300

    update

--- src/pages/Homepage/HomePage.js
+++ src/pages/Homepage/HomePage.js
@@ -1,11 +1,11 @@
 import { Button } from '@material-ui/core'
 import React from 'react'
 
-function HomePage({handleLogOut}) {
+const HomePage = ({handleLogOut}) => {
     return (
         <div>
             <h1>welcome to you page</h1>
-            <Button variant="primary" onclick={handleLogOut}>Log out</Button>
+            <Button onClick={handleLogOut}>Log out</Button>
 
         </div>
     )
