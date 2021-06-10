53ff65072728ce338fb722f83696f58bc57a5c3a
Diana-nyamai <dnyamai.dn@gmail.com>
2021-06-10 13:49:24 +0300

    homepage.js update

--- src/pages/Homepage/HomePage.js
+++ src/pages/Homepage/HomePage.js
@@ -1,10 +1,11 @@
 import { Button } from '@material-ui/core'
 import React from 'react'
+import SideNavBar from './SideNavBar'
 
 const HomePage = ({handleLogOut}) => {
     return (
         <div>
-            <h1>welcome to you page</h1>
+            <SideNavBar/>
             <Button onClick={handleLogOut} variant="contained" color="primary">Log out</Button>
         </div>
     )
