0d1b46b81e6286b609c14e9e0309b1ac66925c56
Diana-nyamai <dnyamai.dn@gmail.com>
2021-06-22 23:50:38 +0300

    update

--- src/pages/Homepage/SideNavBar.js
+++ src/pages/Homepage/SideNavBar.js
@@ -21,7 +21,6 @@ function SideNavBar({handleLogOut}) {
     <Content style={{ padding: '0 50px' }}>
       <Breadcrumb style={{ margin: '16px 0' }}>
         <Breadcrumb.Item>Home</Breadcrumb.Item>
-        <Breadcrumb.Item>List</Breadcrumb.Item>
         <Breadcrumb.Item>App</Breadcrumb.Item>
       </Breadcrumb>
       <div className="site-layout-content"><h1>Welcome To The Homepage</h1></div>
