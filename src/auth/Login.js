3e2ebd452a26a297727ded465fcc867ad19a1f4c
Diana-nyamai <dnyamai.dn@gmail.com>
2021-07-07 22:31:38 +0300

    update

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -37,7 +37,7 @@ const Login = (props) => {
           <TextField required label="Password" type="password" value={password} margin="normal" onChange={(e) => setPassword(e.target.value)}
           endAdornment={
               <InputAdornment>
-              <IconButton onClick={handleClickShowPasswordup}>{values.showPassword ? <Visibility/> : <VisibilityOff />}</IconButton>
+              <IconButton onClick={handleClickShowPassword}>{values.showPassword ? <Visibility/> : <VisibilityOff />}</IconButton>
               </InputAdornment>
 
           }
