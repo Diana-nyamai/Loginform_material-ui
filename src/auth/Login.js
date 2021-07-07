17884ced8bd4a78e4c1a347cb73ca1c8ff0ec8f5
Diana-nyamai <dnyamai.dn@gmail.com>
2021-07-07 21:23:01 +0300

    UPDATE

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import {Button, Grid, Link, TextField, Typography,InputAdornment,IconButton} from '@material-ui/core';
-import {Visibility, Visibilityoff} from '@material-ui/icons';
+import {Visibility, VisibilityOff} from '@material-ui/icons';
 import natur from '../assets/Images/natur.png'
 import logo from '../assets/Images/logo.png'
 
